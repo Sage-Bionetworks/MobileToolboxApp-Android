93c5b2300f94c573d024e1afe759c7447cfba65c
Nathaniel Brown <nathaniel.brown@sagebase.org>
2021-05-17 15:20:59 -0700

    Show today screen instead of AssessmentListActivity

--- alpha-app/src/main/java/org/sagebionetworks/research/mtb/alpha_app/MainActivity.kt
+++ alpha-app/src/main/java/org/sagebionetworks/research/mtb/alpha_app/MainActivity.kt
@@ -7,6 +7,7 @@ import org.koin.android.ext.android.inject
 import org.sagebionetworks.bridge.kmm.presentation.auth.ExternalIdSignInActivity
 import org.sagebionetworks.bridge.kmm.shared.repo.AuthenticationRepository
 import org.sagebionetworks.research.mtb.alpha_app.ui.AssessmentListActivity
+import org.sagebionetworks.research.mtb.alpha_app.ui.today.TodayFragment
 
 class MainActivity :AppCompatActivity() {
 
@@ -20,17 +21,18 @@ class MainActivity :AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         if (!authenticationRepository.isAuthenticated()) {
+            // Show sign-in screen
             val launchIntent = Intent(this, ExternalIdSignInActivity::class.java)
                 .setData(intent.data)
                 .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
 
             startActivity(launchIntent)
         } else {
-            val launchIntent = Intent(this, AssessmentListActivity::class.java)
-                .setData(intent.data)
-                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
-
-            startActivity(launchIntent)
+            // Show Today screen
+            if (supportFragmentManager.fragments.isEmpty()) {
+                supportFragmentManager.beginTransaction().add(android.R.id.content, TodayFragment())
+                    .setReorderingAllowed(true).commit()
+            }
         }
 
     }
