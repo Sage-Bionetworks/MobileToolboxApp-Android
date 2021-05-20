ba3d69a1544c24d04ba930d6520af885dbb31b64
Joshua Liu <liujoshua@users.noreply.github.com>
2021-05-19 17:05:32 -0700

    versionName 0.5, versionCode 6

--- alpha-app/build.gradle.kts
+++ alpha-app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.sagebionetworks.research.mtb.app"
         minSdkVersion(23)
         targetSdkVersion(30)
-        versionCode = 5
-        versionName = "0.4"
+        versionCode = 6
+        versionName = "0.5"
 
         multiDexEnabled = true
         multiDexKeepFile = File("multidex-config.txt")
