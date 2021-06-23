9a7d5d8a566207399027ff4137d00436fa1607aa
Nathaniel Brown <nathaniel.brown@sagebase.org>
2021-06-23 15:58:58 -0700

    Add tabs for Account screen with placeholder fragments.

--- alpha-app/src/main/java/org/sagebionetworks/research/mtb/alpha_app/ui/account/AccountFragment.kt
+++ alpha-app/src/main/java/org/sagebionetworks/research/mtb/alpha_app/ui/account/AccountFragment.kt
@@ -6,8 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import org.sagebionetworks.research.mtb.alpha_app.R
 import org.sagebionetworks.research.mtb.alpha_app.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
@@ -30,15 +34,56 @@ class AccountFragment : Fragment() {
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        accountViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        binding.viewPager.adapter = AccountPagerAdapter(this)
+
+        // Set the icon and text for each tab
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.setIcon(getTabIcon(position))
+            tab.text = getTabTitle(position)
+        }.attach()
+
         return root
+    }
+
+    private fun getTabIcon(position: Int): Int {
+        return when (position) {
+            PROFILE_PAGE_INDEX -> R.drawable.ic_nav_account
+            SETTINGS_PAGE_INDEX -> R.drawable.ic_nav_settings
+            else -> throw IndexOutOfBoundsException()
+        }
+    }
+
+    private fun getTabTitle(position: Int): String? {
+        return when (position) {
+            PROFILE_PAGE_INDEX -> getString(R.string.profile)
+            SETTINGS_PAGE_INDEX -> getString(R.string.settings)
+            else -> null
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+}
+
+const val PROFILE_PAGE_INDEX = 0
+const val SETTINGS_PAGE_INDEX = 1
+
+class AccountPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    /**
+     * Mapping of the ViewPager page indexes to their respective Fragments
+     */
+    private val tabFragmentsCreators: Map<Int, () -> Fragment> = mapOf(
+        PROFILE_PAGE_INDEX to { ProfileFragment() },
+        SETTINGS_PAGE_INDEX to { SettingsFragment() }
+    )
+
+    override fun getItemCount() = tabFragmentsCreators.size
+
+    override fun createFragment(position: Int): Fragment {
+        return tabFragmentsCreators[position]?.invoke() ?: throw IndexOutOfBoundsException()
     }
 }
