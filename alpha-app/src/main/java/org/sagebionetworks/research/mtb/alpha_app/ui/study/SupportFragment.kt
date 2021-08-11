25b56a4c32b568b3e8f9bbc501461232f984a7f3
Nathaniel Brown <nathaniel.brown@sagebase.org>
2021-08-11 10:33:40 -0700

    Viewpager for study tabs

---
+++ alpha-app/src/main/java/org/sagebionetworks/research/mtb/alpha_app/ui/study/SupportFragment.kt
@@ -0,0 +1,19 @@
+package org.sagebionetworks.research.mtb.alpha_app.ui.study
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.sagebionetworks.research.mtb.alpha_app.R
+
+class SupportFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_support, container, false)
+    }
+}
