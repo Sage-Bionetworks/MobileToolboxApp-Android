f61b658bfaf4b2cbbe01ab5f91134f5301fa93e6
Nathaniel Brown <nathaniel.brown@sagebase.org>
2021-09-15 13:11:04 -0700

    Update MTB measures to version 1.2.4.

--- app/src/main/java/org/sagebionetworks/research/mobiletoolbox/app/AppModule.kt
+++ app/src/main/java/org/sagebionetworks/research/mobiletoolbox/app/AppModule.kt
@@ -1,83 +1,24 @@
 package org.sagebionetworks.research.mobiletoolbox.app
 
-import edu.northwestern.mobiletoolbox.common.assessment.MtbNodeStateProvider
-import edu.northwestern.mobiletoolbox.dimensional_change_card_sort.navigation.DCCSNodeStateProvider
-import edu.northwestern.mobiletoolbox.dimensional_change_card_sort.serialization.dccsModuleInfoSerializersModule
-import edu.northwestern.mobiletoolbox.flanker.navigation.FlankerNodeStateProvider
-import edu.northwestern.mobiletoolbox.flanker.serialization.flankerModuleInfoSerializersModule
-import edu.northwestern.mobiletoolbox.fname.navigation.FNAMENodeStateProvider
-import edu.northwestern.mobiletoolbox.fname.serialization.fnameModuleInfoSerializersModule
-import edu.northwestern.mobiletoolbox.mfs.navigation.MfsNodeStateProvider
-import edu.northwestern.mobiletoolbox.mfs.serialization.mfsModuleInfoSerializersModule
-import edu.northwestern.mobiletoolbox.number_match.navigation.NumberMatchNodeStateProvider
-import edu.northwestern.mobiletoolbox.number_match.serialization.numberMatchModuleInfoSerializersModule
-import edu.northwestern.mobiletoolbox.picture_sequence_memory.navigation.PSMNodeStateProvider
-import edu.northwestern.mobiletoolbox.picture_sequence_memory.serialization.psmModuleInfoSerializersModule
-import edu.northwestern.mobiletoolbox.spelling.navigation.SpellingNodeStateProvider
-import edu.northwestern.mobiletoolbox.spelling.serialization.spellingModuleInfoSerializersModule
-import edu.northwestern.mobiletoolbox.vocabulary.dichotomous_engine.VocabNodeStateProvider
-import edu.northwestern.mobiletoolbox.vocabulary.serialization.vocabularyModuleInfoSerializersModule
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.modules.plus
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import org.sagebionetworks.assessmentmodel.AssessmentRegistryProvider
-import org.sagebionetworks.assessmentmodel.navigation.CustomNodeStateProvider
-import org.sagebionetworks.assessmentmodel.presentation.AssessmentFragmentProvider
 import org.sagebionetworks.assessmentmodel.resourcemanagement.FileLoader
-import org.sagebionetworks.assessmentmodel.serialization.EmbeddedJsonAssessmentRegistryProvider
 import org.sagebionetworks.assessmentmodel.serialization.FileLoaderAndroid
-import org.sagebionetworks.assessmentmodel.serialization.nodeSerializersModule
-import org.sagebionetworks.research.mobiletoolbox.app.ui.today.TodayViewModel
 import org.sagebionetworks.bridge.assessmentmodel.upload.AssessmentResultArchiveUploader
 import org.sagebionetworks.bridge.kmm.shared.upload.UploadRequester
 import org.sagebionetworks.research.mobiletoolbox.app.ui.account.AccountViewModel
 import org.sagebionetworks.research.mobiletoolbox.app.ui.history.HistoryViewModel
 import org.sagebionetworks.research.mobiletoolbox.app.ui.login.LoginViewModel
 import org.sagebionetworks.research.mobiletoolbox.app.ui.study.StudyInfoViewModel
+import org.sagebionetworks.research.mobiletoolbox.app.ui.today.TodayViewModel
 
 val appModule = module {
-
-    single<AssessmentRegistryProvider> {
-        EmbeddedJsonAssessmentRegistryProvider(
-            get(), "embedded_assessment_registry",
-            Json {
-                serializersModule = nodeSerializersModule +
-                        dccsModuleInfoSerializersModule +
-                        flankerModuleInfoSerializersModule +
-                        mfsModuleInfoSerializersModule +
-                        spellingModuleInfoSerializersModule +
-                        vocabularyModuleInfoSerializersModule +
-                        fnameModuleInfoSerializersModule +
-                        numberMatchModuleInfoSerializersModule +
-                        psmModuleInfoSerializersModule
-                isLenient = true
-            },
-        )
-    }
 
     single<AssessmentResultArchiveUploader> {
         MtbAssessmentResultArchiveUploader(get(), get(), get(), get())
     }
 
     single { UploadRequester(get(), get()) }
-
-    single<AssessmentFragmentProvider> { MtbAssessmentFragmentProvider() }
-    single<CustomNodeStateProvider> {
-        MtbAppNodeStateProvider(
-            listOf(
-                VocabNodeStateProvider(get()),
-                FlankerNodeStateProvider(get()),
-                SpellingNodeStateProvider(get()),
-                DCCSNodeStateProvider(get()),
-                NumberMatchNodeStateProvider(get()),
-                MfsNodeStateProvider(get()),
-                FNAMENodeStateProvider(get()),
-                PSMNodeStateProvider(get()),
-                MtbNodeStateProvider(get()),
-            )
-        )
-    }
 
     factory<FileLoader> { FileLoaderAndroid(get()) }
     viewModel { TodayViewModel(get(), get(), get()) }
--- app/src/main/java/org/sagebionetworks/research/mobiletoolbox/app/MtbApplication.kt
+++ app/src/main/java/org/sagebionetworks/research/mobiletoolbox/app/MtbApplication.kt
@@ -1,17 +1,8 @@
 package org.sagebionetworks.research.mobiletoolbox.app
 
 import androidx.multidex.MultiDexApplication
+import edu.northwestern.mobiletoolbox.assessments_provider.mtbModules
 import edu.northwestern.mobiletoolbox.bridge.MTBKitCore
-import edu.northwestern.mobiletoolbox.common.di.mtbKoinModule
-import edu.northwestern.mobiletoolbox.dichotomous_engine.di.dichotomousKoinModule
-import edu.northwestern.mobiletoolbox.dimensional_change_card_sort.koin.dccsKoinModule
-import edu.northwestern.mobiletoolbox.flanker.di.flankerKoinModule
-import edu.northwestern.mobiletoolbox.fname.koin.fnameKoinModule
-import edu.northwestern.mobiletoolbox.mfs.di.mfsKoinModule
-import edu.northwestern.mobiletoolbox.number_match.koin.numberMatchKoinModule
-import edu.northwestern.mobiletoolbox.picture_sequence_memory.koin.psmKoinModule
-import edu.northwestern.mobiletoolbox.spelling.di.spellingKoinModule
-import edu.northwestern.mobiletoolbox.vocabulary.di.vocabularyKoinModule
 import net.danlew.android.joda.JodaTimeAndroid
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -36,19 +27,8 @@ class MtbApplication : MultiDexApplication(), KoinComponent {
             androidContext(this@MtbApplication)
             workManagerFactory()
             modules(presentationModule)
-            modules(
-                appModule,
-                mtbKoinModule,
-                mfsKoinModule,
-                dccsKoinModule,
-                dichotomousKoinModule,
-                vocabularyKoinModule,
-                spellingKoinModule,
-                flankerKoinModule,
-                numberMatchKoinModule,
-                fnameKoinModule,
-                psmKoinModule
-            )
+            modules(appModule)
+            modules(mtbModules())
         }
 
         MTBKitCore.boot(this)
