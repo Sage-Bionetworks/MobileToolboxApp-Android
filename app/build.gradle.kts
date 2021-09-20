f6d9a71284360b7f2b678035bb511805d723c40e
Nathaniel Brown <nathaniel.brown@sagebase.org>
2021-09-20 15:35:49 -0700

    Version 0.9.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.sagebionetworks.research.mobiletoolbox.app"
         minSdkVersion(23)
         targetSdkVersion(30)
-        versionCode = 2
-        versionName = "0.8.$versionCode"
+        versionCode = 3
+        versionName = "0.9.$versionCode"
 
         multiDexEnabled = true
         multiDexKeepFile = File("multidex-config.txt")
