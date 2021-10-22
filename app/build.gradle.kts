f0082b0354f3aa231bd0bb96840881d8881e2e1b
Joshua Liu <liujoshua@users.noreply.github.com>
2021-10-22 06:57:31 -0700

    V0.13.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.sagebionetworks.research.mobiletoolbox.app"
         minSdkVersion(23)
         targetSdkVersion(30)
-        versionCode = 6
-        versionName = "0.12.$versionCode"
+        versionCode = 7
+        versionName = "0.13.$versionCode"
 
         multiDexEnabled = true
         multiDexKeepFile = File("multidex-config.txt")
