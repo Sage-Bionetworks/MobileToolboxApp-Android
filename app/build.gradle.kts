34a7c3b5a78688a7f5a5782cdb2b4c1003465c11
Nathaniel Brown <nathaniel.brown@sagebase.org>
2023-01-03 12:58:52 -0800

    Version 0.24.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "org.sagebionetworks.research.mobiletoolbox.app"
         minSdk = 23
         targetSdk = 33
-        versionCode = 25
+        versionCode = 26
         versionName = "0.24.$versionCode"
 
         multiDexEnabled = true
