1a4458787fbbd2b2e7bc7f9c4b9370f0f24f62ea
Nathaniel Brown <nathaniel.brown@sagebase.org>
2023-03-08 16:26:02 -0800

    testSavingAssessmentResult now tests 5 assessments and verifies correct files are in zip file.

--- app/src/androidTest/java/org/sagebionetworks/research/mobiletoolbox/app/MtbRootAssessmentViewModelTest.kt
+++ app/src/androidTest/java/org/sagebionetworks/research/mobiletoolbox/app/MtbRootAssessmentViewModelTest.kt
@@ -1,8 +1,6 @@
 package org.sagebionetworks.research.mobiletoolbox.app
 
 import android.content.Context
-import android.os.Handler
-import android.os.Looper
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -19,24 +17,29 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
 import org.koin.core.component.inject
 import org.sagebionetworks.assessmentmodel.AssessmentRegistryProvider
 import org.sagebionetworks.assessmentmodel.AssessmentResultCache
 import org.sagebionetworks.assessmentmodel.navigation.CustomNodeStateProvider
-import org.sagebionetworks.assessmentmodel.navigation.FinishedReason
-import org.sagebionetworks.assessmentmodel.navigation.SaveResults
+import org.sagebionetworks.assessmentmodel.presentation.AssessmentViewModel
 import org.sagebionetworks.assessmentmodel.serialization.AssessmentInfoObject
 import org.sagebionetworks.assessmentmodel.serialization.AssessmentPlaceholderObject
+import org.sagebionetworks.bridge.assessmentmodel.upload.AndroidUploadEncryptor
 import org.sagebionetworks.bridge.assessmentmodel.upload.AssessmentResultArchiveUploader
+import org.sagebionetworks.bridge.kmm.shared.cache.loadResource
 import org.sagebionetworks.bridge.kmm.shared.models.AdherenceRecord
 import org.sagebionetworks.bridge.kmm.shared.repo.AdherenceRecordRepo
 import org.sagebionetworks.bridge.kmm.shared.repo.AuthenticationRepository
-import org.sagebionetworks.research.mobiletoolbox.app.MtbRootAssessmentViewModel
+import org.sagebionetworks.bridge.kmm.shared.upload.UploadFile
 import org.sagebionetworks.research.mobiletoolbox.app.recorder.RecorderRunner
 import org.sagebionetworks.research.mobiletoolbox.app.recorder.model.recorderConfigJsonCoder
 import org.sagebionetworks.research.mobiletoolbox.app.ui.login.PermissionPageType
+import java.io.File
+import java.io.OutputStream
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
+import java.util.zip.ZipInputStream
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -49,7 +52,14 @@ class MtbRootAssessmentViewModelTest : KoinComponent {
     //Documentation if we want to insert different implementations for test as needed
     //https://insert-koin.io/docs/reference/koin-android/instrumented-testing
 
-    private val archiveUploader: AssessmentResultArchiveUploader by inject()
+
+
+    private val archiveUploader: AssessmentResultArchiveUploader = AssessmentResultArchiveUploader(get(), get(), get(),
+        object : AndroidUploadEncryptor {
+            override fun encrypt(stream: OutputStream): OutputStream {
+                return stream
+            }
+        })
     private val adherenceRecordRepo: AdherenceRecordRepo by inject()
     private val recorderRunnerFactory: RecorderRunner.RecorderRunnerFactory by inject()
     private val assessmentResultCache: AssessmentResultCache by inject()
@@ -125,11 +135,49 @@ class MtbRootAssessmentViewModelTest : KoinComponent {
         // Verify that there are no pending uploads
         assertFalse(archiveUploader.uploadRequester.pendingUploads)
 
+        // Northwestern measures that don't have lateInit engines
+        val measureIds = listOf(
+            "Picture Sequence MemoryV1",
+            "Number Match",
+            "Flanker Inhibitory Control",
+            "FNAME Learning Form 1",
+            "FNAME Test Form 1")
+
         val studyId = "androidTestStudyId"
-        // TODO: Figure out best assessment to use for testing or testing multiple -nbrown 2/20/23
-        val assessmentInfo = AssessmentInfoObject("Flanker Inhibitory Control")
+        for (assessmentId in measureIds) {
+            runAssessment(studyId, assessmentId, "$assessmentId instanceGuid")
+        }
+
+
+        // Check that upload got written to disk
+        val pendingUploads = archiveUploader.uploadRequester.getPendingFileUploads()
+        assertEquals(measureIds.size, pendingUploads.size)
+
+        // Check that archives have expected files
+        for (uploadResource in pendingUploads) {
+            val expectedFiles = setOf("taskData.json", "metadata.json", "info.json", "motion.json")
+            verifyArchive(uploadResource.loadResource()!!, expectedFiles)
+        }
+
+        // Check the adherence records
+        runBlocking {
+            val recordsMap = adherenceRecordRepo.getAllCachedAdherenceRecords(studyId).first()
+            assertEquals(measureIds.size, recordsMap.size)
+            for (entry in recordsMap.entries) {
+                val records = entry.value
+                assertEquals(1, records.size)
+                val record = records[0]
+                assertTrue(record.declined)
+                assertNotNull(record.startedOn)
+                assertNull(record.finishedOn)
+            }
+        }
+
+    }
+
+    private fun runAssessment(studyId: String, assessmentId: String, assessmentInstanceId: String) {
+        val assessmentInfo = AssessmentInfoObject(assessmentId)
         val assessmentPlaceholder = AssessmentPlaceholderObject(assessmentInfo.identifier, assessmentInfo)
-        val assessmentInstanceId = "testInstanceGuid"
         val eventTimestamp = Clock.System.now()
         val adherenceRecord = AdherenceRecord(assessmentInstanceId, eventTimestamp.toString())
 
@@ -147,21 +195,16 @@ class MtbRootAssessmentViewModelTest : KoinComponent {
             studyId = studyId)
 
         val branchNodeState = getValue(viewModel.assessmentLoadedLiveData)
-
         assertNotNull(branchNodeState)
         branchNodeState?.rootNodeController = viewModel
+
+        val assessmentViewModel = AssessmentViewModel(branchNodeState!!)
 
         recorderRunnerFactory.withConfig(recorderConfigJsonCoder.decodeFromString(recorderConfig))
         viewModel.startRecorderRunner()
 
-        //TODO: Figure out navigating through assessment to generate more results -nbrown 2/20/23
-        branchNodeState?.exitEarly(
-            FinishedReason.Incomplete(
-                SaveResults.Now,
-                markFinished = false,
-                declined = true
-            )
-        )
+        assessmentViewModel.start()
+        assessmentViewModel.declineAssessment()
 
         assertNotNull(viewModel.saveJob)
         val saveJobLatch = CountDownLatch(1)
@@ -173,21 +216,23 @@ class MtbRootAssessmentViewModelTest : KoinComponent {
         // Check that saving job is done
         assertTrue(viewModel.saveJob?.isCompleted == true)
 
-        // Check that upload got written to disk
-        assertTrue(archiveUploader.uploadRequester.pendingUploads)
+    }
 
-        // Check the adherence record
-        runBlocking {
-            val recordsMap = adherenceRecordRepo.getAllCachedAdherenceRecords(studyId).first()
-            assertEquals(1, recordsMap.size)
-            val records = recordsMap.entries.first().value
-            assertEquals(1, records.size)
-            val record = records[0]
-            assertTrue(record.declined)
-            assertNotNull(record.startedOn)
-            assertNull(record.finishedOn)
-        }
+    private fun verifyArchive(uploadFile: UploadFile, expectedFiles: Set<String>) {
+        val mutableExpectedFiles = expectedFiles.toMutableSet()
 
+        val zipFile = File(uploadFile.filePath)
+
+        val zipInputStream = ZipInputStream((zipFile.inputStream()))
+
+        do {
+            val entry = zipInputStream.nextEntry
+            if (entry != null) {
+                assertTrue("Entry : ${entry.name} not found in expected list of files", mutableExpectedFiles.remove(entry.name))
+            }
+        } while (entry != null)
+
+        assertTrue(mutableExpectedFiles.isEmpty())
     }
 }
 
@@ -207,3 +252,4 @@ internal fun <T> getValue(liveData: LiveData<T>): T? {
     liveData.removeObserver(observer)
     return data
 }
+
