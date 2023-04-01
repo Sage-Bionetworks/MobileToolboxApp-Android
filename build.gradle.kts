15a63fc4c0561a761f4071c9dfd6fdcdc039de3c
Nathaniel Brown <nathaniel.brown@sagebase.org>
2023-04-01 12:25:14 -0700

    Update google services to hopefully fix building release

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
-        classpath("com.google.gms:google-services:4.3.10")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
+        classpath("com.google.gms:google-services:4.3.15")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
